1ed9256f51b50435dda5cf7978fc92bd4a2f4ffc
Yiyi Zhang <zhan0665@algonquinlive.com>
2020-10-12 10:44:00 +0800

    Completes assignment

---
+++ script.js
@@ -0,0 +1,12 @@
+let $container = document .getElementById('container')
+
+let container = []
+
+for (let i =9203; <=127855; i++) {
+    container .push(`
+    <p style='text-align: center;'>
+    <span style="font-size: 2rem;">&#${i};</span><br>
+    <container>${i}</code></p>`)
+}
+
+$container.innerHTML = $codes.innerHTML + codes.loin('')
